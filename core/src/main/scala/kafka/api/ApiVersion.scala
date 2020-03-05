ba040862b6ff86c77769ac444060c271e3904173
Lucas Wang <luwang@linkedin.com>
2020-03-05 11:36:30 -0800

    [LI-HOTFIX] Use max broker epoch in controller requests TICKET = KAFKA-7186 LI_DESCERIPTION = Currently the controller is creating a separate copy of UpdateMetadataRequest for each broker in the cluster, which may cause OOMs upon controller startup. This patch is the first step toward making the UpdateMetadataRequests cacheable. We change the broker-specific epoch in control requests to a common maximum broker epoch seen in all brokers, such that all UpdateMetadataRequests have the same payload.
    EXIT_CRITERIA = KAFKA-7186

--- core/src/main/scala/kafka/api/ApiVersion.scala
+++ core/src/main/scala/kafka/api/ApiVersion.scala
@@ -94,7 +94,9 @@ object ApiVersion {
     // Add adding_replicas and removing_replicas fields to LeaderAndIsrRequest
     KAFKA_2_4_IV0,
     // Flexible version support in inter-broker APIs
-    KAFKA_2_4_IV1
+    KAFKA_2_4_IV1,
+    // Add cacheable broker epoch to UpdateMetadataRequest
+    KAFKA_2_5_IV0
   )
 
   // Map keys are the union of the short and full versions
@@ -320,19 +322,27 @@ case object KAFKA_2_3_IV1 extends DefaultApiVersion {
   val id: Int = 23
 }
 
+case object KAFKA_2_3_IV2 extends DefaultApiVersion {
+  val shortVersion: String = "2.3"
+  val subVersion = "IV2"
+  val recordVersion = RecordVersion.V2
+  val id: Int = 24
+}
+
 case object KAFKA_2_4_IV0 extends DefaultApiVersion {
   val shortVersion: String = "2.4"
   val subVersion = "IV0"
   val recordVersion = RecordVersion.V2
-  val id: Int = 24
+  val id: Int = 25
 }
 
 case object KAFKA_2_4_IV1 extends DefaultApiVersion {
   val shortVersion: String = "2.4"
   val subVersion = "IV1"
   val recordVersion = RecordVersion.V2
-  val id: Int = 25
+  val id: Int = 26
 }
+
 
 object ApiVersionValidator extends Validator {
 
